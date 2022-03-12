5fc08563c7d3443312dc65122239573d064bcfad
usman-0002 <chmusman1999@gmail.com>
2022-03-12 12:37:38 -0800

    Dev-05 Created Product Controller   1. Created Product controllers along with acitons and routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     root 'devise/sessions#new'
   end
 
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
