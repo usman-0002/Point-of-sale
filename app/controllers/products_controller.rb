5b98264eb0a41dcb8bdabb78fd95923e16c40bdd
usman-0002 <chmusman1999@gmail.com>
2022-03-13 12:26:41 -0700

    Dev-07 Product Create functionality   1. Added create new product functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,16 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: 'Product added Successfully!'
+    else
+      render :new, alert: 'failed to add product!'
+    end
   end
 
   def edit
@@ -19,5 +26,10 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit!
   end
 end
