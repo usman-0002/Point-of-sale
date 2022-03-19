01772643bfab928fac7a1851e3900cbe352d45e1
usman-0002 <chmusman1999@gmail.com>
2022-03-19 02:58:28 -0700

    Dev-10 Active Storage   1. Install active storage to store product image in local

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_one_attached :avatar, dependent: :destroy
+
   validates :code, :name, presence: true
   validates :code, uniqueness: true
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
